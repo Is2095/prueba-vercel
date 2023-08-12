a92fa2999dc1b32d01bb1c36c729ed357246c553
Ismael <ismael2095951@gmail.com>
2023-08-12 20:55:39 -0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port =  3001;
 
-app.listen(port, () => {console.log(`listen on PORT ${port}`)});
+app.listen(port, () => console.log(`listen on PORT ${port}`));
