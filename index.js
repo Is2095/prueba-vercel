ec72a732b68b858dc06ac013a6fe21087c1b0930
Ismael <ismael2095951@gmail.com>
2023-08-12 21:01:23 -0300

    add

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const app = express();
 
 const port =  3001;
 
+app.get("/", (req, res) => {
+    res.send("la página de inicio ");
+})
 app.listen(port, () => console.log(`listen on PORT ${port}`));
