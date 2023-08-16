73e4a0abb1b2617f39522214b2e52d057146386a
Ismael <ismael2095951@gmail.com>
2023-08-16 15:10:37 -0300

    prueba

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const app = require("./src/app");
-
+const connectMongo = require("./src/DB_connection");
 const port =  3001;
-
+connectMongo()
 app.listen(port, () => console.log(`listen on PORT ${port}`));
--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const Favorite = require("./models/Favorite");
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("la página de inicio ");
+})
+app.get("/rick", async (req, res) => {
+    try {
+        const favAll = await Favorite.find()
+        console.log(favAll, '*********************');
+        return res.status(200).json(favAll)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
 })
 
 module.exports = app
